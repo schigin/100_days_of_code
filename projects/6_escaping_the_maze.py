ab245e1092dd6334e1573a356e42af583f4d86e4
Sergey Chigin <sergey.chigin@exness.com>
2023-06-04 16:34:26 +0300

    day_6: added script for final project

---
+++ projects/6_escaping_the_maze.py
@@ -0,0 +1,23 @@
+# go to link:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# the solution for the escaping the maze game is below
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+# move him until the wall
+# and turn him counter-clockwise
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
